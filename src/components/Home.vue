ebc3278dfc206e0b7dbdd21aa0f48d0b8eda5652
Jessica Chung <jchung722@gmail.com>
2019-10-03 20:37:42 -0500

    update home page and add create auction button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="hello center mt-20">
-    <h1 class="text-6xl">{{ msg }}</h1>
-    <img class="object-center m-auto" src="../assets/construction.png">
+  <div class="hello">
+    <div class="mx-auto shadow bg-cover bg-bottom pt-20" style="background-image: url(https://fleabay.net/wp-content/uploads/2019/07/Best-Flea-Markets-Marrakesh-1.jpg)">
+      <div class="text-center text-white pb-10">
+        <h1 class="text-6xl pt-5">{{ msg }}</h1>
+      </div>
+      <router-link to="/auctions/new" class="inline-block text-3xl px-5 py-3 mb-10 leading-none border rounded border-transparent bg-white hover:bg-black hover:text-white mt-4 lg:mt-0">Create An Auction!</router-link>
+    </div>
   </div>
 </template>
 
@@ -15,21 +19,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
