c42b32834d7f5f70b37a1c0b20fc330c80774b87
Jessica Chung <jchung722@gmail.com>
2019-10-10 10:36:04 -0500

    add auction card component to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,55 @@
 <template>
-  <div class="hello">
+  <div>
     <div class="mx-auto shadow bg-cover bg-bottom pt-20" style="background-image: url(https://fleabay.net/wp-content/uploads/2019/07/Best-Flea-Markets-Marrakesh-1.jpg)">
       <div class="text-center text-white pb-10">
         <h1 class="text-6xl pt-5">Welcome to fleaBay</h1>
       </div>
       <router-link to="/auctions/new" class="button inline-block text-3xl px-5 py-3 mb-10 leading-none border rounded border-transparent bg-white hover:bg-black hover:text-white mt-4 lg:mt-0">Create An Auction!</router-link>
     </div>
+
+    <div class="text-red-500 errors" v-if="error">{{ error }}</div>
+    <AuctionCard
+      v-for="auction in auctions"
+      v-bind:key="auction.id"
+      v-bind:auctionId="auction.id"
+      v-bind:name="auction.name"
+      v-bind:imageUrl="checkPicture(auction)"
+      v-bind:bid="auction.starting_bid"
+      v-bind:endDate="auction.end_date"
+    ></AuctionCard>
   </div>
 </template>
 
 <script>
+import fleaBayApi from '../services/fleaBayApi'
+import AuctionCard from '@/components/AuctionCard'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    AuctionCard
+  },
+  data () {
+    return {
+      auctions: [],
+      error: ''
+    }
+  },
+  created () {
+    fleaBayApi.getAllAuctions()
+      .then(response => {
+        this.auctions = response.data
+      })
+      .catch(error => this.setError(error, 'Error retrieving auction.'))
+  },
+  methods: {
+    setError (error, text) {
+      this.error = (error.response && error.response.data && error.response.data.error) || text
+    },
+    checkPicture (auction) {
+      const placeholderUrl = 'https://static.thenounproject.com/png/1174579-200.png'
+      return auction.picture || placeholderUrl
+    }
+  }
 }
 </script>
--- test/unit/specs/components/Home.spec.js
+++ test/unit/specs/components/Home.spec.js
@@ -1,14 +1,39 @@
 import { mount, RouterLinkStub } from '@vue/test-utils'
 import Home from '@/components/Home'
 
+let wrapper
+
+beforeEach(() => {
+  wrapper = mount(Home, {
+    stubs: { 'router-link': RouterLinkStub }
+  })
+})
+
+afterEach(() => {
+  wrapper.destroy()
+})
+
 describe('Home.vue', () => {
   it('router link directs to auction form path', () => {
-    const wrapper = mount(Home, {
-      stubs: { 'router-link': RouterLinkStub }
-    })
     const auctionFormPath = '/auctions/new'
     const routerLink = wrapper.find(RouterLinkStub)
 
     expect(routerLink.props().to).toBe(auctionFormPath)
+  })
+
+  it('renders errors on page', () => {
+    wrapper.setData({
+      error: 'TEST ERROR'
+    });
+
+    expect(wrapper.find('.errors').text()).toEqual('TEST ERROR')
+  })
+
+  it('returns placeholder if object does not have picture url', () => {
+    expect(wrapper.vm.checkPicture({ picture: '' })).toEqual('https://static.thenounproject.com/png/1174579-200.png')
+  })
+
+  it('returns picture url if object has one', () => {
+    expect(wrapper.vm.checkPicture({ picture: 'image url' })).toEqual('image url')
   })
 })
